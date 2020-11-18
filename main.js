150cf1b1f404bf89aeb2e7cdd56c47cfe3bc1a5a
Gian <73497721+GFsolari@users.noreply.github.com>
2020-11-18 15:04:18 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,31 @@
+let miImage = document.querySelector("img");
+miImage.onclick = function () {
+    let miSrc = miImage.getAttribute("src");
+    if (miSrc === "img/hola.jpeg") {
+      miImage.setAttribute("src","img/hola2.png");
+    } else {
+      miImage.setAttribute("src", "img/hola.jpeg");
+    }
+}
+// hola fran
+/* hopla 
+fdgdfg
+*/ 
+let miBoton = document.querySelector("button");
+let miTitulo = document.querySelector("h1");
+
+function estableceNombreUsuario() {
+    let miNombre = prompt("Por favor, ingresa tu nombre.");
+    localStorage.setItem("nombre", miNombre);
+    miTitulo.textContent = "Mozilla es genial," + miNombre;
+}
+    if (!localStorage.getItem("nombre")) {
+    estableceNombreUsuario();
+}
+    else {
+    let nombreAlmacenado = localStorage.getItem("nombre");
+    miTitulo.textContent = "Mozilla es genial," + nombreAlmacenado;
+}
+miBoton.onclick = function() {
+    estableceNombreUsuario();
+}
